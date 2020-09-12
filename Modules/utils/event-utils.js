d8d0358f0920809be7494e528858a48562de1056
Perin <joaovperin@live.com>
2020-09-11 23:16:50 -0300

    Here we go (again5)

--- Modules/utils/event-utils.js
+++ Modules/utils/event-utils.js
@@ -6,73 +6,74 @@
 if (!window.TwFramework) {
     window.TwFramework = {}
 }
+new Promise(exportModule => {
+    (() => {
+        'use strict';
 
-(() => {
-    'use strict';
+        // Namespace/module
+        const ModuleName = 'event.event-utils';
 
-    // Namespace/module
-    const ModuleName = 'event.event-utils';
+        // Public API
+        $.extend(window.TwFramework, {
+            onVisibilityChange: (callback) => _visibilityChangeListeners.push(callback)
+        });
 
-    // Public API
-    $.extend(window.TwFramework, {
-        onVisibilityChange: (callback) => _visibilityChangeListeners.push(callback)
-    });
+        const _visibilityChangeListeners = [];
 
-    const _visibilityChangeListeners = [];
+        // Visibility change checker
+        (function (_onChangeFn) {
+            var hidden = "hidden";
 
-    // Visibility change checker
-    (function (_onChangeFn) {
-        var hidden = "hidden";
-
-        // Standards:
-        if (hidden in document)
-            document.addEventListener("visibilitychange", onchange);
-        else if ((hidden = "mozHidden") in document)
-            document.addEventListener("mozvisibilitychange", onchange);
-        else if ((hidden = "webkitHidden") in document)
-            document.addEventListener("webkitvisibilitychange", onchange);
-        else if ((hidden = "msHidden") in document)
-            document.addEventListener("msvisibilitychange", onchange);
-        // IE 9 and lower:
-        else if ("onfocusin" in document)
-            document.onfocusin = document.onfocusout = onchange;
-        // All others:
-        else
-            window.onpageshow = window.onpagehide = window.onfocus = window.onblur = onchange;
-
-        function onchange(evt) {
-            var v = "visible",
-                h = "hidden",
-                evtMap = {
-                    focus: v,
-                    focusin: v,
-                    pageshow: v,
-                    blur: h,
-                    focusout: h,
-                    pagehide: h
-                };
-
-            evt = evt || window.event
-            if (!evt) evt = {};
-            if (evt.type in evtMap)
-                evt.windowStatus = evtMap[evt.type];
+            // Standards:
+            if (hidden in document)
+                document.addEventListener("visibilitychange", onchange);
+            else if ((hidden = "mozHidden") in document)
+                document.addEventListener("mozvisibilitychange", onchange);
+            else if ((hidden = "webkitHidden") in document)
+                document.addEventListener("webkitvisibilitychange", onchange);
+            else if ((hidden = "msHidden") in document)
+                document.addEventListener("msvisibilitychange", onchange);
+            // IE 9 and lower:
+            else if ("onfocusin" in document)
+                document.onfocusin = document.onfocusout = onchange;
+            // All others:
             else
-                evt.windowStatus = this[hidden] ? "hidden" : "visible";
-            // Fire event listeners assynchronously
-            const _evtData = {
-                hasFocus: (evt.windowStatus === 'visible')
-            };
-            _visibilityChangeListeners.forEach(_listener => _listener(_evtData));
-        }
+                window.onpageshow = window.onpagehide = window.onfocus = window.onblur = onchange;
 
-        // set the initial state (but only if browser supports the Page Visibility API)
-        if (document[hidden] !== undefined)
-            onchange({
-                type: document[hidden] ? "blur" : "focus"
-            });
+            function onchange(evt) {
+                var v = "visible",
+                    h = "hidden",
+                    evtMap = {
+                        focus: v,
+                        focusin: v,
+                        pageshow: v,
+                        blur: h,
+                        focusout: h,
+                        pagehide: h
+                    };
+
+                evt = evt || window.event
+                if (!evt) evt = {};
+                if (evt.type in evtMap)
+                    evt.windowStatus = evtMap[evt.type];
+                else
+                    evt.windowStatus = this[hidden] ? "hidden" : "visible";
+                // Fire event listeners assynchronously
+                const _evtData = {
+                    hasFocus: (evt.windowStatus === 'visible')
+                };
+                _visibilityChangeListeners.forEach(_listener => _listener(_evtData));
+            }
+
+            // set the initial state (but only if browser supports the Page Visibility API)
+            if (document[hidden] !== undefined)
+                onchange({
+                    type: document[hidden] ? "blur" : "focus"
+                });
+            return exportModule(true);
+        })();
+
+        console.log("[TwFramework] ~> Module '", ModuleName, "' successfully loaded!");
         return true;
     })();
-
-    console.log("[TwFramework] ~> Module '", ModuleName, "' successfully loaded!");
-    return true;
-})();
+});
--- Modules/utils/notify-utils.js
+++ Modules/utils/notify-utils.js
@@ -6,62 +6,65 @@
 if (!window.TwFramework) {
     window.TwFramework = {}
 }
+new Promise(exportModule => {
 
-(async (ModuleLoader) => {
+    (async (ModuleLoader) => {
 
-    // Namespace/module
-    const ModuleName = 'utils.notify-utils';
+        // Namespace/module
+        const ModuleName = 'utils.notify-utils';
 
-    // Dependency loading
-    await ModuleLoader.loadModule('utils.event-utils');
+        // Dependency loading
+        await ModuleLoader.loadModule('utils.event-utils');
 
-    /** Script configurations */
-    const _Configurations = {};
+        /** Script configurations */
+        const _Configurations = {};
 
-    // Public API
-    $.extend(window.TwFramework, {
-        setIdleTitlePreffix: (preffix, originalTitle) => {
-            if (_Configurations.setIdleTitlePreffix) {
-                return console.error('Cannot set idle title 2 times!');
-            }
-
-            // Update title with preffix
-            const _updateTitlefn = evt => {
-                if (!evt) return;
-                const _originalTitle = _Configurations.setIdleTitlePreffix.originalTitle;
-                if (evt.hasFocus) document.title = _originalTitle;
-                else document.title = `[${preffix}] ${_originalTitle}`;
-            };
-
-            // Set the event listener and call the function on the window load
-            TwFramework.onVisibilityChange(_updateTitlefn);
-            _updateTitlefn({
-                hasFocus: false
-            });
-
-            // Saves settings on the global scope
-            $.extend(_Configurations, {
-                setIdleTitlePreffix: {
-                    originalTitle: originalTitle
+        // Public API
+        $.extend(window.TwFramework, {
+            setIdleTitlePreffix: (preffix, originalTitle) => {
+                if (_Configurations.setIdleTitlePreffix) {
+                    return console.error('Cannot set idle title 2 times!');
                 }
-            });
+
+                // Update title with preffix
+                const _updateTitlefn = evt => {
+                    if (!evt) return;
+                    const _originalTitle = _Configurations.setIdleTitlePreffix.originalTitle;
+                    if (evt.hasFocus) document.title = _originalTitle;
+                    else document.title = `[${preffix}] ${_originalTitle}`;
+                };
+
+                // Set the event listener and call the function on the window load
+                TwFramework.onVisibilityChange(_updateTitlefn);
+                _updateTitlefn({
+                    hasFocus: false
+                });
+
+                // Saves settings on the global scope
+                $.extend(_Configurations, {
+                    setIdleTitlePreffix: {
+                        originalTitle: originalTitle
+                    }
+                });
+            }
+        });
+
+        // Confirm the script is ready!
+        console.log("[TwFramework] ~> Module '", ModuleName, "' successfully loaded!");
+        return exportModule(true);
+    })({
+        loadModule: name => {
+            const modulePath = name.replace('.', '/');
+            return new Promise((resolve, reject) => {
+                $.ajax({
+                        method: "GET",
+                        url: `https://raw.githubusercontent.com/joaovperin/TribalWars/master/Modules/${modulePath}.js`,
+                        dataType: "text"
+                    })
+                    .done(res => resolve(await eval(res)))
+                    .fail(req => reject(console.error("Fail loading module '", name, "'.")));
+            })
         }
     });
 
-    // Confirm the script is ready!
-    console.log("[TwFramework] ~> Module '", ModuleName, "' successfully loaded!");
-    return true;
-})({
-    loadModule: async name => {
-        const modulePath = name.replace('.', '/');
-        return await new Promise((resolve, reject) => {
-            $.ajax({
-                    method: "GET",
-                    url: `https://raw.githubusercontent.com/joaovperin/TribalWars/master/Modules/${modulePath}.js`,
-                    dataType: "text"
-                })
-                .done(async res => resolve(await eval(res)))
-                .fail(req => reject(console.error("Fail loading module '", name, "'.")));
-        })
-    }
 });
