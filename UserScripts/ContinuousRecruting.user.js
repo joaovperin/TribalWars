9ccb4712ffc03f5bec161b77aaed4cda2cf8d111
Perin <joaovperin@live.com>
2020-09-12 00:00:42 -0300

    Inc. version

--- UserScripts/ContinuousRecruting.user.js
+++ UserScripts/ContinuousRecruting.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                Continuous Recruiting
-// @version     	    1.0.3
+// @version     	    1.0.4
 // @description         Auto-recruit units if there's no one on the queue
 // @author              joaovperin
 // @icon                https://i.imgur.com/7WgHTT8.gif
